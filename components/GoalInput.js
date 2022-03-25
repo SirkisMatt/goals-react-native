6137ae378493ea05a63f8d1babf15601d65a9cbd
SirkisMatt <sirkis.matt@gmail.com>
2022-03-25 18:37:59 +1300

    can create and delete goals

---
+++ components/GoalInput.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  Modal,
+  Image,
+} from "react-native";
+
+function GoalInput(props) {
+  const { onAddGoal, modalIsVisible, onEndGoal } = props;
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <Modal visible={modalIsVisible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/river.jpg")}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={onEndGoal} color="#f31282" />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Add Goal"
+              onPress={() => addGoalHandler(enteredGoalText)}
+              color="#b180f0"
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#311b6b",
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: 20,
+    borderRadius: 75,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#efd0ff",
+    backgroundColor: "#efd0ff",
+    borderRadius: 6,
+    color: "#120438",
+    width: "100%",
+    padding: 16,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
